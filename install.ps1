08a3d2cc4e9f4cbeb41297785cb7451494abc7fa
kostyamessanger <kostyagameshelp@vk.ru>
2026-01-18 14:56:00 +0500

    Delete install.ps1

--- install.ps1
+++
@@ -1,8 +0,0 @@
-$installDir = "C:\gnpm"
-New-Item -ItemType Directory -Path $installDir -Force | Out-Null
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/kostyamessanger/gnpm/main/gnpm.js" -OutFile "$installDir\gnpm.js"
-$path = [System.Environment]::GetEnvironmentVariable("Path", "User")
-if ($path -notlike "*$installDir*") {
-  $newPath = "$path;$installDir"
-  [System.Environment]::SetEnvironmentVariable("Path", $newPath, "User")
-}
